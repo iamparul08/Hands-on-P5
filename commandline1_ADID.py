0714660a9a17c04b175be5b239e096354daec431
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-19 09:55:53 +0530

    Add files via upload

---
+++ commandline1_ADID.py
@@ -0,0 +1,7 @@
+n1_ADID = input("Enter 100")
+print(n1_ADID)
+n2_ADID = input("Enter 200")
+print(n2_ADID)
+
+sum_ADID = int(n1_ADID) + int(n2_ADID)
+print(sum_ADID)
---
+++ commandline2_ADID.py
@@ -0,0 +1,7 @@
+s1_ADID = input("Enter Wipro")
+s2_ADID = input("Enter Technologies")
+
+
+sum1_ADID = s1_ADID + s2_ADID
+print(sum1_ADID)
+print("Data type of the commandline arguments is String")
